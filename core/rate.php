6990b1f94ce1601e3aa419daa61e049014ab848b
Adeola Boye <89581536+adeoladev@users.noreply.github.com>
2022-01-26 22:18:32 +0000

    Update rate.php

--- core/rate.php
+++ core/rate.php
@@ -1,26 +1,39 @@
 <?php
 
 require 'connect.php';
+session_start();
 
-$rating = mysqli_real_escape_string($conn,$_GET['rating']);
+$rating = $_GET['rating'];
 $username = $_GET['username'];
 $movie = $_GET['movie'];
 $date = date("F j, Y h:i a");
 
-$stmt = $conn->prepare("INSERT INTO ratings (username, imdb_id, rating, date) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("ssis", $username, $movie, $rating, $date);
-$stmt->execute();
-$stmt->close();
-$conn->close();
+if (!isset($_SESSION['username'])) {
+    header("HTTP/1.0 404 Not logged in.");
+    exit();
+} else if ($rating > 10 || $rating < 0 || empty($rating)) {
+    header("HTTP/1.0 404 Incorrect Values.");
+    exit(); 
+}
 
-if (mysqli_connect_errno()) {
-    header("HTTP/1.0 404".mysqli_connect_error());
+$already = $conn->query("SELECT * FROM ratings WHERE username='$username' AND imdb_id = '$movie'") or die($conn->error);
+
+if ($already->num_rows>0) {
+    $stmt = $conn->prepare("UPDATE ratings SET rating = ?, date = ?  WHERE imdb_id = ? AND username = ?");
+    $stmt->bind_param("isss", $rating, $date, $movie, $username);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    header("HTTP/1.0 200 Success");
     exit();
 } else {
+    $stmt = $conn->prepare("INSERT INTO ratings (username, imdb_id, rating, date) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssis", $username, $movie, $rating, $date);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
     header("HTTP/1.0 200 Success");
     exit();
 }
-  
-
 
 ?>
